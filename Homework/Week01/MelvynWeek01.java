43e3b81b700627b319d4d419bbf74293d9262adb
melvyniandrag <melvyndrag@yahoo.com>
2019-08-29 00:49:19 -0400

    done with setting up week 1 homework assignment

--- Homework/Week01/MelvynWeek01.java
+++ Homework/Week01/MelvynWeek01.java
@@ -6,13 +6,23 @@ public class MelvynWeek01{
         };
 
     public int[] getMyIntArray(){
+        System.out.println("Getting int array . . .");
         return myIntArr;
+    }
+
+    public void useAnOrComparator(){
+        System.out.println("Using or comparator . . .");
+        int x = 1;
+        if( (x > 1) || ( x == 1 ) ){
+            System.out.println("Successfully used an 'or' comparison");
+        }
     }
 
     public static void main( String[] arguments ){
         System.out.println("Starting to run homework code");
         MelvynWeek01 homework = new MelvynWeek01();
         int[] intArr = homework.getMyIntArray();
+        homework.useAnOrComparator();
         System.out.println("Finished running homework code");
     }
 }

b53c1644b4f009bcb2c024af6a6a9e5a304da56c
melvyniandrag <melvyniandrag@gmail.com>
2019-10-03 11:21:27 -0400

    fixed spacing

--- Homework/Week05/BitManipulator.java
+++ Homework/Week05/BitManipulator.java
@@ -1,48 +1,45 @@
-
-
 public class BitManipulator{
-	
-	/*
-	 * Funky function that makes a short from 4 bytes.
-	 *
-	 * @param byteArray - byte[] of length 4.
-	 * @return a short made from the last 4 bits of every byte in byteArray.
-	 *
-	 */
-	private static short extractShort(byte[] byteArray) throws ByteArrayLengthException {
-		if( byteArray.length != 4 ){
-			throw new ByteArrayLengthException("parameter byteArray to 'extractShort' must have length 4.");
-		}
-		else{
-			// TODO implement this part.
-			short s = 1;
-			return s;
-		}
-	}
+    /*
+     * Funky function that makes a short from 4 bytes.
+     *
+     * @param byteArray - byte[] of length 4.
+     * @return a short made from the last 4 bits of every byte in byteArray.
+     *
+     */
+    private static short extractShort(byte[] byteArray) throws ByteArrayLengthException {
+        if( byteArray.length != 4 ){
+            throw new ByteArrayLengthException("parameter byteArray to 'extractShort' must have length 4.");
+        }
+        else{
+            // TODO implement this part.
+            short s = 1;
+            return s;
+        }
+    }
 
-	private static byte[] intToByteArray(int i){
-		//TODO convert 'i' to a byte array. Remove this 'dummy' stuff.
-		byte[] dummy = { (byte)0x19, (byte)0x01, (byte)0x7A, (byte)0xAF };
-		return dummy;
-	}
+    private static byte[] intToByteArray(int i){
+        //TODO convert 'i' to a byte array. Remove this 'dummy' stuff.
+        byte[] dummy = { (byte)0x19, (byte)0x01, (byte)0x7A, (byte)0xAF };
+        return dummy;
+    }
 
-	public static void main(String[] args){
-		int i = 56401;
-		byte[] byteArr = intToByteArray(i);
-		try{
-			short s = extractShort(byteArr);
-	        	System.out.println(String.valueOf(s));
-		}
-		catch( Exception e ){
-			System.out.println(e.getMessage());
-		}
-	}	
+    public static void main(String[] args){
+        int i = 56401;
+        byte[] byteArr = intToByteArray(i);
+        try{
+            short s = extractShort(byteArr);
+                        System.out.println(String.valueOf(s));
+        }
+        catch( Exception e ){
+            System.out.println(e.getMessage());
+        }
+    }    
 
 }
 
 class ByteArrayLengthException extends Exception{
-	public ByteArrayLengthException( String s ){
-		super(s);
-	}
+    public ByteArrayLengthException( String s ){
+        super(s);
+    }
 }
 

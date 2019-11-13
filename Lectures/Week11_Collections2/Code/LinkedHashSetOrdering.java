094f0ad22f39d93db837dd1ea83c80627e0ca1d2
melvyniandrag <melvyniandrag@gmail.com>
2019-11-13 14:54:42 -0500

    added lecture and code

---
+++ Lectures/Week11_Collections2/Code/LinkedHashSetOrdering.java
@@ -0,0 +1,34 @@
+/**
+ * Linked Hash Sets do maintain insertion order! 
+ */
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetOrdering{
+	public static void main(String[] args){
+		Set<String> s = new LinkedHashSet<String>();
+		boolean b8 = s.add("Haha");
+		System.out.println(b8);
+		boolean b4 = s.add("World\n");
+		System.out.println(b4);
+		boolean b5 = s.add("World\n");
+		System.out.println(b5);
+		boolean b6 = s.add(new String("World\n"));
+		System.out.println(b6);
+
+		boolean b1 = s.add(new String("Hello "));
+		System.out.println(b1);
+		boolean b2 = s.add(new String("Hello "));
+		System.out.println(b2);
+		boolean b3 = s.add("Hello ");
+		System.out.println(b3);
+
+		for( String str : s ){
+			System.out.print(str);
+		}
+
+		System.out.println(s.toString());
+
+	}
+}
+

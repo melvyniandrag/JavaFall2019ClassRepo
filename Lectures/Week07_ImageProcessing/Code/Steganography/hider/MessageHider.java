972b4479ff1adef33e2bc189e5c63caf4239d876
melvyniandrag <melvyniandrag@gmail.com>
2020-08-07 12:13:05 -0400

    updated steganography stuff

--- Lectures/Week07_ImageProcessing/Code/Steganography/hider/MessageHider.java
+++ Lectures/Week07_ImageProcessing/Code/Steganography/hider/MessageHider.java
@@ -5,6 +5,15 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 public class MessageHider{
+	
+	/**
+	 * Read a png image file into a 2D array of ARGB values.
+	 *
+	 * @param fileName - the name of the image we want to read in
+	 * @return A 2D array of ARGB values in row major order.
+	 * @throws NoAlphaChannelException if the image being read has no Alpha channel.
+	 * @throws IOException if the image can't be read.
+	 */
 	public static int[][] imageTo2DArr(String fileName) throws NoAlphaChannelException, IOException {
 		BufferedImage image = ImageIO.read(MessageHider.class.getResource(fileName));
 		final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
@@ -36,39 +45,70 @@ public class MessageHider{
 		}
 		return result;
 	}
-	
+
+
+	/**
+	 * Hide an array of bytes in a 2d byte array.
+	 *
+	 * @param toHide - byte array containing the message to hide
+	 * @param imageArray - 2D byte array containing pixel vals
+	 * @return void - imageArray is modified in place
+	 *
+	 * For each byte in imageArray, replace the last bit with a bit 
+	 * from toHide until the bits are exhausted. At the time of
+	 * writing this code, imageArray contains 4 byte ints. The bytes
+	 * represent the ARGB channels ( a byte each for A,R,G,B ). As
+	 * such, as we replace the last bit in each int in imageArray
+	 * we are creating a minor change in the Blue channel of the
+	 * image. This minor change will be unnoticeable to the human
+	 * eye. 
+	 *
+	 * This is a cool function from the students' perspective
+	 * because we use bitwise operators like &= and |=, 
+	 * and we modify a 2d array in place because Java
+	 * is pass by value, which in this case behaves like pass by
+	 * reference because of the way Java works. All these Java 
+	 * syntax tidbits are combined with the cool practical thing
+	 * that we are doing steganography.
+	 */	
 	public static void HideBytes( byte[] toHide, int[][] imageArray){
 		int bitIdx = 0;
-		int bitsPrinted = 0;
 		for( int row = 0; row < imageArray.length; row++ ){
 			for ( int col = 0; col < imageArray[row].length; ++col ) {
-				imageArray[row][col] &= 0xFFFFFFFE; // set the last blue bit to zero.a
 				int bitToHide = getBit(bitIdx, toHide);
-				if(bitToHide != 0x02){
-					System.out.print(bitToHide);	
-					bitsPrinted++;
-					if( bitsPrinted == 8 ){
-						System.out.print(" ");
-						bitsPrinted = 0;
-					}
-				}
-				imageArray[row][col] |= bitToHide; // set the last blue bit to zero.
+				// clear the last bit by setting it to zero
+				imageArray[row][col] &= 0xFFFFFFFE;
+				// now set the last bit to the bit we want to hide.
+				imageArray[row][col] |= bitToHide;
 				bitIdx++;	
 			}
 		}
-		System.out.println();
-		// Note the lack of return value here. In Java, if you modify an array in a function
-		// the array is modified in place.
-		// The reasoning for this requires some discussion and deals with pass by value / pass by reference	
 	}
 
+	
+	/**
+	 * Return 0 or 1 representing the bit at bitIdx in toHide
+	 * 
+	 * @param bitIdx - the index of the bit we want to extract.
+	 * @param toHide - the sequence of bytes we extract bits from.
+	 * @return 0 if the bit at position bitIdx is 0
+	 * @return 1 if the bit at position bitIdx is 1
+	 * @return BIT_INDEX_OUT_OF_RANGE_ERR if we request a bit out of range
+	 *
+	 * If we want to extact bit 0 from array [ 10000000b, 00000000b ] we will
+	 * return '1'. To extract the 1 from arr = [ 00000000b, 01000000b ] we would call
+	 * getBit(9, arr).
+	 * 
+	 * A call like getBit( 18, arr ), if arr = [ 00000000b, 01000001b ] will return 
+	 * BIT_INDEX_OUT_OF_RANGE_ERR as there aren't enough bits in the array to get bit 18.
+	 */
 	private static int getBit( int bitIdx, byte[] toHide){
+		final int BIT_INDEX_OUT_OF_RANGE_ERROR = 0x02;
 		int byteIdx = bitIdx / 8;
 		if( byteIdx >= toHide.length){
-			return 0x02;
+			return BIT_INDEX_OUT_OF_RANGE_ERROR;	
 		}
 		int localBitIdx = bitIdx % 8;
-		//System.out.println(String.format("Getting bit %d from byte %d at localIndex %d", bitIdx, byteIdx, localBitIdx));
 		byte current = toHide[byteIdx];
 		current >>>= ( 7 - localBitIdx );
 		return 0x00000001 & current; 
@@ -76,24 +116,39 @@ public class MessageHider{
 	
 
 	/**
-	 * Return the last bit from the first 8 * numBytesInMessage bytes
+	 * Print the last bit from the first 8 * numBytesInMessage bytes
+	 * 
+	 * @param imageArray - a row-major-ordered 2D array of ARGB ints representing an image.
+	 * @param numBytesInMessage - the number of bytes hidden in the image array
+	 * @return void - no return value
+	 * @return TOO_MANY_BYTES_ERROR ( todo ) - error if the number of hidden bytes exceeds
+	 *                                         the image size
+	 * 
+	 * Print to stdout the binary encoded bytes found in the image.
 	 */
 	public static void extractMessage(int[][] imageArray, int numBytesInMessage){
-		System.out.println("Last bits from each pixel of the imageArray are:");
+		System.out.println("Bytes extracted: ");
 		int numBits = 8 * numBytesInMessage;
 		for( int row = 0; row < imageArray.length; row++){
 			for( int col = 0; col < imageArray[row].length; col++){
 				if( row * imageArray[row].length + col == numBits ){
-					System.out.println("Did these bits agree with the bits we set when we hid a message?");
+					// We found all the bits, let's get out of here.
 					return;
 				}
 				else{
-					System.out.println(String.format("0x%08X", (0x00000001 & imageArray[row][col])));
+					System.out.print(String.format("%d", (0x00000001 & imageArray[row][col])));
+					if((row*imageArray[0].length + col + 1) % 8 == 0)
+						System.out.print("\n");
 				}
 			}
 		}
 	}
 
+	/**
+	 * Write the 2D array color as a png image.
+	 * 
+	 * @param color - 2D array representing the ARGB values in an image.
+	 */
 	public static void writeImage(int[][] color) {
 		String path = "output.png";
 		BufferedImage image = new BufferedImage(color[0].length, color.length, BufferedImage.TYPE_INT_ARGB);
@@ -110,7 +165,11 @@ public class MessageHider{
 		}
 	}
 }
-	
+
+
+/**
+ * Custom exception thrown when there is no Alpha Channel in an image
+ */	
 class NoAlphaChannelException extends Exception{
 	NoAlphaChannelException(String s){
 		super(s);
